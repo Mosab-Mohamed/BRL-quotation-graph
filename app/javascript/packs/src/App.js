d35eb71fe500dac00f62fad759f6085986e5018a
Mosab Muhammad <mosab.muhammad.kh@gmail.com>
2019-02-16 20:16:09 +0200

    add homwpage and change react app source file

---
+++ app/javascript/packs/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Hello React</div>;
+  }
+}
+
+export default App;
